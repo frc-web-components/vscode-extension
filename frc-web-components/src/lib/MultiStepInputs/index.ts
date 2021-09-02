b434377546d80b5e0b6c97c8310f743506d4ee48
Amory Galili <amorygalili@gmail.com>
2021-09-02 17:18:42 -0400

    Multistep inputs are restarted when opened

--- frc-web-components/src/lib/MultiStepInputs/index.ts
+++ frc-web-components/src/lib/MultiStepInputs/index.ts
@@ -117,6 +117,7 @@ export default class MultiStepInputs {
     }
 
     public show(): void {
+        this.restart();
         const { inputStep } = this.inputSteps[this.currentStep];
         inputStep.show();
     }
